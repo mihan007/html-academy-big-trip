18b2a92fb925b946c5d2785c1c7acafc5b4f043e
Mikhail Kuklin <mihan007@ya.ru>
2021-06-26 09:34:15 +0300

    Refactor variable names

--- src/view/point-list.js
+++ src/view/point-list.js
@@ -7,37 +7,40 @@ dayjs.extend(durationPlugin);
 
 export const createPointListTemplate = (point) => {
   const { typeName, typeIconUrl, offers, destination, startDate, endDate, isFavorite, price } = point;
-  const startDateForAttribute = dayjs(startDate).format(DateTimeFormat.htmlDateTimeAttribute);
-  const endDateForAttribute = dayjs(endDate).format(DateTimeFormat.htmlDateTimeAttribute);
-  const startDateTime = dayjs(startDate).format(DateTimeFormat.pointListTime);
-  const startDateLabel = dayjs(startDate).format(DateTimeFormat.pointListDate);
-  const endDateTime = dayjs(endDate).format(DateTimeFormat.pointListTime);
+  const startDateFormTemplate = dayjs(startDate).format(DateTimeFormat.htmlDateTimeAttribute);
+  const startDateTimeTemplate = dayjs(startDate).format(DateTimeFormat.pointListTime);
+  const startDateLabelTemplate = dayjs(startDate).format(DateTimeFormat.pointListDate);
+
+  const endDateFormTemplate = dayjs(endDate).format(DateTimeFormat.htmlDateTimeAttribute);
+  const endDateTimeTemplate = dayjs(endDate).format(DateTimeFormat.pointListTime);
+
   const duration = dayjs.duration(dayjs(endDate).diff(dayjs(startDate)));
-  const durationFormatted = duration.hours() > 0
+  const durationTemplate = duration.hours() > 0
     ? duration.format(DateTimeFormat.durationWithHours)
     : duration.format(DateTimeFormat.durationWithoutHours);
-  const offersList = createOffersTemplate(offers);
+
+  const offersListTemplate = createOffersTemplate(offers);
   const favoriteClassName = isFavorite ? 'event__favorite-btn--active' : '';
 
   return `<li class="trip-events__item">
               <div class="event">
-                <time class="event__date" datetime="2019-03-18">${startDateLabel}</time>
+                <time class="event__date" datetime="2019-03-18">${startDateLabelTemplate}</time>
                 <div class="event__type">
                   <img class="event__type-icon" width="42" height="42" src="${typeIconUrl}" alt="${typeName} icon">
                 </div>
                 <h3 class="event__title">${typeName} ${destination.title}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
-                    <time class="event__start-time" datetime="${startDateForAttribute}">${startDateTime}</time>
+                    <time class="event__start-time" datetime="${startDateFormTemplate}">${startDateTimeTemplate}</time>
                     —
-                    <time class="event__end-time" datetime="${endDateForAttribute}">${endDateTime}</time>
+                    <time class="event__end-time" datetime="${endDateFormTemplate}">${endDateTimeTemplate}</time>
                   </p>
-                  <p class="event__duration">${durationFormatted}</p>
+                  <p class="event__duration">${durationTemplate}</p>
                 </div>
                 <p class="event__price">
                   €&nbsp;<span class="event__price-value">${price}</span>
                 </p>
-                ${offersList}
+                ${offersListTemplate}
                 <button class="event__favorite-btn ${favoriteClassName}" type="button">
                   <span class="visually-hidden">Add to favorite</span>
                   <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
