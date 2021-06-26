ff9f36dced0b1564191b0f3d26fcc8e7c31017a8
Mikhail Kuklin <mihan007@ya.ru>
2021-06-26 09:21:38 +0300

    Remove non required functions

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-import dayjs from 'dayjs';
-
 // Функция из интернета по генерации случайного числа из диапазона
 // Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 export const getRandomInteger = (a = 0, b = 1) => {
@@ -7,20 +5,4 @@ export const getRandomInteger = (a = 0, b = 1) => {
   const upper = Math.floor(Math.max(a, b));
 
   return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-export const isTaskExpired = (dueDate) => {
-  return dueDate === null ? false : dayjs().isAfter(dueDate, 'D');
-};
-
-export const isTaskExpiringToday = (dueDate) => {
-  return dueDate === null ? false : dayjs(dueDate).isSame(dayjs(), 'D');
-};
-
-export const isTaskRepeating = (repeating) => {
-  return Object.values(repeating).some(Boolean);
-};
-
-export const humanizeTaskDueDate = (dueDate) => {
-  return dayjs(dueDate).format('D MMMM');
 };
