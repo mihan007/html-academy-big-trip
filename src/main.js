b189497e076bc5248011a89c86f7b8fbb72f2548
Mikhail Kuklin <mihan007@ya.ru>
2021-08-03 18:43:30 +0300

    3.1.1 Отрефакторит рендер-метод
    - Перенесём render-функцию в модуль с утилитами и переименуем её в renderTemplate
    - Опишем render-функцию для отрисовки элемента, которая в следствии станет главной render-функцией
    - Добавим вспомогательную функцию для создания DOM-элемента

--- src/main.js
+++ src/main.js
@@ -6,32 +6,28 @@ import { createFilterTemplate } from './view/filter';
 import { createInfoTemplate } from './view/info';
 import { createSortTemplate } from './view/sort';
 import { generatePoint } from './mock/point';
-import { range } from './utils';
+import { range, renderTemplate } from './utils';
 
 const POINT_COUNT = 20;
 const points = range(1, POINT_COUNT).map(() => generatePoint());
 
-const render = (container, template, place = 'beforeend') => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const menuElement = document.querySelector('.js-menu');
-render(menuElement, createMenuTemplate());
+renderTemplate(menuElement, createMenuTemplate());
 
 const contentElement = document.querySelector('.js-content');
-render(contentElement, createPointFormTemplate(points[0]));
-render(contentElement, createPointFormTemplate());
+renderTemplate(contentElement, createPointFormTemplate(points[0]));
+renderTemplate(contentElement, createPointFormTemplate());
 
-points.map((el, index) => render(contentElement, createPointListTemplate(points[index])));
+points.map((el, index) => renderTemplate(contentElement, createPointListTemplate(points[index])));
 
 const tripElement = document.querySelector('.js-trip');
-render(tripElement, createInfoTemplate(points));
+renderTemplate(tripElement, createInfoTemplate(points));
 
 const costElement = document.querySelector('.js-cost');
-render(costElement, createCostTemplate(points));
+renderTemplate(costElement, createCostTemplate(points));
 
 const filterElement = document.querySelector('.js-filter');
-render(filterElement, createFilterTemplate());
+renderTemplate(filterElement, createFilterTemplate());
 
 const sortElement = document.querySelector('.js-sort');
-render(sortElement, createSortTemplate());
+renderTemplate(sortElement, createSortTemplate());
